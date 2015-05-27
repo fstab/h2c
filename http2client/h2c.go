0a083bb12964dea5d8ef3531d770610d9dc5c3e7
Fabian Stäber <fabian@fstab.de>
2015-05-27 17:53:39 +0200

    first connection to server established

--- http2client/h2c.go
+++ http2client/h2c.go
@@ -1,7 +1,9 @@
 package http2client
 
 import (
+	"crypto/tls"
 	"fmt"
+	"strings"
 )
 
 type Http2Client struct {
@@ -12,6 +14,18 @@ func New() *Http2Client {
 }
 
 func (h2c *Http2Client) Connect(url string) (string, error) {
-	fmt.Printf("Connecting to %v\n", url)
+	if url[0:8] != "https://" {
+		return "", fmt.Errorf("Only https:// urls are currently supported.")
+	}
+	host := strings.Split(url[8:], "/")[0]
+	if !strings.Contains(host, ":") {
+		host = host + ":443"
+	}
+	fmt.Printf("Connecting to %v\n", host)
+	conn, err := tls.Dial("tcp", host, &tls.Config{})
+	if err != nil {
+		return "", fmt.Errorf("Failed to connect to %v: %v", host, err.Error())
+	}
+	conn.Close()
 	return "", nil
 }
