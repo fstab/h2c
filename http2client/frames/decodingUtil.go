6fa59fd9e04edc19038306276983cf8328565c0a
Fabian Stäber <fabian@fstab.de>
2015-10-05 21:36:25 +0200

    simplified decoding util

--- http2client/frames/decodingUtil.go
+++ http2client/frames/decodingUtil.go
@@ -1,17 +1,18 @@
 package frames
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
 
+// uint32_ignoreFirstBit is like binary.BigEndian.Uint32, but with some differences:
+//  * data may have an arbitrary length:
+//  ** If the length is < four bytes, the leading bytes are assumed to be zero.
+//  ** If the length is > four bytes, the leading bytes are ignored.
+//  * The first Bit is ignored.
 func uint32_ignoreFirstBit(data []byte) uint32 {
-	buffer := make([]byte, 4)
-	i := 0
-	for i < 4 && i < len(data) {
+	buffer := make([]byte, 4) // implicitly initialized with zero bytes
+	for i := 0; i < 4 && i < len(data); i++ {
 		buffer[len(buffer)-(1+i)] = data[len(data)-(1+i)]
-		i++
-	}
-	for i < 4 {
-		buffer[len(buffer)-(1+i)] = 0
-		i++
 	}
 	buffer[0] = buffer[0] & 0x7F // clear first bit
 	return binary.BigEndian.Uint32(buffer)
