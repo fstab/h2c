9e2375de34a094e03898ef214e94a2e55a68269d
Fabian Stäber <fabian@fstab.de>
2015-12-14 10:57:43 +0100

    fixed vendor/ content

--- get-vendor-sources.sh
+++ get-vendor-sources.sh
@@ -6,13 +6,15 @@ export GOPATH="$GOPATH/src/github.com/fstab/h2c/vendor"
 rm -rf "$GOPATH"
 mkdir "$GOPATH"
 
-# go get github.com/fstab/h2c
-go get golang.org/x/net/http2/hpack
-go get github.com/fatih/color
+go get github.com/fstab/h2c
+# go get golang.org/x/net/http2/hpack
+# go get github.com/fatih/color
 
 find "$GOPATH" -name '.git' | while read dir ; do rm -rf "$dir" ; done
 ls "$GOPATH" | grep -v src | while read dir ; do rm -rf "$GOPATH/$dir" ; done
 rm -rf "$GOPATH/src/github.com/fstab/h2c"
+mv "$GOPATH"/src/* "$GOPATH"
+rmdir "$GOPATH"/src
 
 echo LAST UPDATE: `date` > "$GOPATH/LAST_UPDATE.txt"
 
